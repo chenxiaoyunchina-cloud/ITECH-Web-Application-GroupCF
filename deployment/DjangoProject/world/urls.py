1e0a45b5a45ad1bed36bf7bcace339963e93fc3e
Abdullah Tariq <abdullahbtariq97@gmail.com>
2026-03-05 03:19:51 +0000

    added cities search, add and manage urls in world management to manage and append new cities

--- deployment/DjangoProject/world/urls.py
+++ deployment/DjangoProject/world/urls.py
@@ -5,4 +5,7 @@ app_name = "world"
 
 urlpatterns = [
     path("cities/", views.city_list, name="city_list"),
+    path("cities/search/", views.city_search, name="city_search"),
+    path("cities/add/", views.city_add, name="city_add"),
+    path("cities/manage/", views.city_manage, name="city_manage"),
 ]
