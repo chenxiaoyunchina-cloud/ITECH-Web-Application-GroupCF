f0513dd8fded9c91a340a28f28f94616d46cf7ad
Abdullah Tariq <abdullahbtariq97@gmail.com>
2026-03-05 03:18:07 +0000

    added new urls for register, login and logout. It was only admin or moderator login previously and only admin could create accounts.

--- deployment/DjangoProject/accounts/urls.py
+++ deployment/DjangoProject/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
+    path("", views.home, name="home"),
+
+    path("register/", views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path("me/", views.me, name="me"),
     path("me/city/", views.select_city, name="select_city"),
 ]
