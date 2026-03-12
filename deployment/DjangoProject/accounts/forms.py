6060397e131d582e65508b20bf20e1dcaedac42c
Qian Yao <3097045Y@student.gla.ac.uk>
2026-03-12 00:03:10 +0000

    Improve login UI and Bootstrap form styling

--- deployment/DjangoProject/accounts/forms.py
+++ deployment/DjangoProject/accounts/forms.py
@@ -1,11 +1,55 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter your email"
+        })
+    )
 
     class Meta:
         model = get_user_model()
         fields = ("username", "email", "password1", "password2")
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Choose a username"
+        })
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Create a password"
+        })
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Confirm your password"
+        })
+    )
+
+
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter your username"
+        })
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter your password"
+        })
+    )
